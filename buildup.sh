0737fce1626ae8f3cb9f28eceb9d7f61eb627354
ESONHUGH <32677240+Esonhugh@users.noreply.github.com>
2022-04-30 18:18:17 +0800

    add main password change tools building in shell script

--- buildup.sh
+++ buildup.sh
@@ -1,18 +1,26 @@
+mkdir output
+mkdir output/admintool
+
 function build_main_web() {
     go build -o output/webserver ./main.go
 }
 
 function build_export_tools() {
-    go build -o output/exporter ./admin/export.go
+    go build -o output/admintool/exporter ./admin/export.go
 }
 
 function build_clean_tool() {
-    go build -o output/cleaner ./admin/clean.go
+    go build -o output/admintool/cleaner ./admin/clean.go
+}
+
+function build_password_tool() {
+    go build -o output/admintool/passchanger ./admin/passRecovery.go
 }
 
 function build_tools() {
     build_clean_tool
     build_export_tool
+    build_password_tool
 }
 
 function  build() {
@@ -20,7 +28,6 @@ function  build() {
     build_tools
 }
 
-mkdir output
 build
 cp ./application.yaml ./output/
 echo "build Success... Look your ./output/ dir"
